da3257760288ae148a806e16e9d77bb88940fa41
Douglas Useda <douglas.useda2@gmail.com>
2022-12-14 14:00:00 -0300

    Adiciona as rotas de Usuário

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ServicoController;
+use App\Http\Controllers\UsuarioController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -22,9 +23,13 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-// Rotas para trabalhar com serviços
+// Rotas de Serviços
 Route::get('/servicos', [ServicoController::class, 'index'])->name('servicos.index');
 Route::get('/servicos/create', [ServicoController::class, 'create'])->name('servicos.create');
 Route::post('/servicos', [ServicoController::class, 'store'])->name('servicos.store');
 Route::get('/servicos/{servico}/edit', [ServicoController::class, 'edit'])->name('servicos.edit');
 Route::put('/servicos/{servico}', [ServicoController::class, 'update'])->name('servicos.update');
+
+
+// Rotas de Usuário
+Route::resource('usuarios', UsuarioController::class);
